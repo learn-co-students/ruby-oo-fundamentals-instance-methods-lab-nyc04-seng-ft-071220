077fbe759814fce02d420a495eb30bdac97b74fa
hortencia c <hortenciaecisneros@gmail.com>
2020-08-04 22:44:58 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog 
+  def bark
+    puts "Woof!"
+  end
+  
+  def sit 
+    puts "The Dog is sitting"
+  end
+    
+end
+
+# define a "class "Dog make sure its in caps ! and make it do something like "bark " ! and create a realistic thing a dog would do like say "Woof" ! 
+# add each seprately like so "def" bark def sit and end end on each !
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  
+  def walk 
+    puts "The Person is walking"
+  end
+  
+end
+
+# notes same as you did in "doc class , create a "class" "person" and define what a person would do.
+#  a person walks , and talks "some should not"! make sure to use def for every "instance method toy create this will change later"!
